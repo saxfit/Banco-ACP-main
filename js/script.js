0f3a12871f824aa00f1d9eb2d1563ef485f933be
CotEuge <148403042+CotEuge@users.noreply.github.com>
2024-05-07 00:00:06 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,78 @@
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+	nro_doc: /^\d{8,8}$/, // 8 numeros
+	pasaporte: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]/, //3 letras y 6 numeros
+}
+const campos = {
+	nombre: false,
+	apellido: false,
+	correo: false,
+	telefono: false
+}
+
+const validacionForm = (e) => {
+	switch (e.target.name) {
+
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+			break;
+		case "apellido":
+			validarCampo(expresiones.apellido, e.target, 'apellido');
+			break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+			break;
+		case "telefono":
+			validarCampo(expresiones.telefono, e.target, 'telefono');
+			break;
+	}
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if (expresion.test(input.value)) {
+		document.getElementById(`grupo_${campo}`).classList.remove(`formulario_grupo-incorrecto`);
+		document.getElementById(`grupo_${campo}`).classList.add(`formulario_grupo-correcto`);
+		document.querySelector(`#grupo_${campo} i`).classList.add(`fa-check-circle`);
+		document.querySelector(`#grupo_${campo} i`).classList.remove(`fa-times-circle`);
+		document.querySelector(`#grupo_${campo} .formulario_input-error`).classList.remove(`formulario_input-error-activo`);
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo_${campo}`).classList.add(`formulario_grupo-incorrecto`);
+		document.getElementById(`grupo_${campo}`).classList.remove(`formulario_grupo-correcto`);
+		document.querySelector(`#grupo_${campo} i`).classList.add(`fa-times-circle`);
+		document.querySelector(`#grupo_${campo} i`).classList.remove(`fa-check-circle`);
+		document.querySelector(`#grupo_${campo} .formulario_input-error`).classList.add(`formulario_input-error-activo`);
+		campos[campo] = false;
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validacionForm);
+	input.addEventListener('blur', validacionForm);
+})
+
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
+})
+/*
+if(campos.nombre && campos.apellido && campos.correo && campos.telefono){
+	formulario.reset();
+
+	document.getElementById('formulario_mensaje-exito').classList.add('formulario_mensaje-exito-activo');
+	setTimeout(() => {
+		document.getElementById('formulario_mensaje-exito').classList.remove('formulario_mensaje-exito-activo');
+	}, 5000);
+
+	document.querySelectorAll('.formulario_grupo-correcto').forEach((icono) => {
+		icono.classList.remove('formulario_grupo-correcto');
+	});
+} else {
+	document.getElementById('formulario_mensaje').classList.add('formulario_mensaje-activo')
+}
+*/
